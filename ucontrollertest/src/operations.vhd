b276db22bb95ab3509caaeb15b373f01f52eed71
Overlord <theexplosivesheep@gmail.com>
2013-04-30 11:16:12 +0200

    fix

--- ucontrollertest/src/operations.vhd
+++ ucontrollertest/src/operations.vhd
@@ -58,7 +58,7 @@ begin
     end process;
 
 -- Combinatorical logic
-process( OP, OP_D, OP_S, PC_INT, OP_SC, OP_DC, ADDER, TG_SS, LD ) begin
+process( OP, OP_D, OP_S, PC_INT, OP_SC, OP_DC, ADDER, IO_D, IO_S, C ) begin
     OP_D_N <= OP_D;
     IO_D_N <= IO_D;
     PC_N <= std_logic_vector( unsigned( PC_INT ) + 1 ); -- Increment program counter pr default;
