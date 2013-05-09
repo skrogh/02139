570d3aaebafd0ae5c6d6eeafce06109af42ed0b8
Overlord <theexplosivesheep@gmail.com>
2013-05-09 13:30:22 +0200

    fix compiler string length

--- project1/java/src/lol.java
+++ project1/java/src/lol.java
@@ -4,7 +4,7 @@ public class lol {
 	
 	public static void main(String[] args) {
 		for ( int i = 0; i < 256; i++ ) {
-			System.out.println( "\"" + toBinary((int)(Math.random() * 256)) + "\" " + "WHEN " + "\"" + toBinary(i) + "\",");
+			System.out.println( "\"" + toBinary((int)(Math.random() * 8192)) + "\" " + "WHEN " + "\"" + toBinary10(i) + "\",");
 		}
 		
 		System.out.println(toBinary(16));
@@ -19,9 +19,27 @@ public class lol {
 			num /= 2;
 		}
 		
-		for( int i = 0; i < 8; i++ ) {
+		for( int i = 0; i < 13; i++ ) {
 			i = out.length();
-			if(i>=8)
+			if(i>=13)
+				break;
+			out = out + "0";
+		}
+		
+		return new StringBuffer(out).reverse().toString();
+	}
+	
+public static String toBinary10( int num ) {
+		
+		String out = "";
+		while( num != 0 ) {
+			out = out + num % 2;
+			num /= 2;
+		}
+		
+		for( int i = 0; i < 10; i++ ) {
+			i = out.length();
+			if(i>=10)
 				break;
 			out = out + "0";
 		}
