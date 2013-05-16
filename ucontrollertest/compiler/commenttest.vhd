21298364aa879c5f6ba21fbe6e1286c4b65ba4c3
overlord <theexplosivesheep@gmail.com>
2013-05-16 06:39:10 +0200

    add multiline comments, multiline whitespace, labels and gotos to compiler

---
+++ ucontrollertest/compiler/commenttest.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+entity rom is
+port( addr : in std_logic_vector( 9 downto 0 );
+do : out std_logic_vector( 12 downto 0 ) );
+end rom;
+architecture behavioural of rom is
+begin
+with addr select
+do <= "0000000000000" WHEN "0000000000",
+"0000000000000" when others;
+end behavioural;
